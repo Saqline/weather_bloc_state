ab975530c410175c9a8ceb64ea562539b4c4f49f
imran nur <imrannurint@gmail.com>
2024-07-16 13:11:13 +0600

    bloc state

--- lib/data/repository/weather_repository.dart
+++ lib/data/repository/weather_repository.dart
@@ -10,7 +10,7 @@ class WeatherRepository {
 
   Future<WeatherModel> getCurrentWeather() async {
     try {
-      String cityName = 'New Delhi';
+      String cityName = 'Dhaka';
       final weatherData = await weatherDataProvider.getCurrentWeather(cityName);
 
       final data = jsonDecode(weatherData);
